278dbb6ccbc12e46f7d20a928d5bbd48824104b5
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-04-26 01:48:56 -0300

    login

--- src/main/java/com/ccp/rest/api/spring/exceptions/handler/CcpRestApiExceptionHandlerSpring.java
+++ src/main/java/com/ccp/rest/api/spring/exceptions/handler/CcpRestApiExceptionHandlerSpring.java
@@ -1,6 +1,8 @@
 package com.ccp.rest.api.spring.exceptions.handler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.ccp.business.CcpBusiness;
 import com.ccp.constantes.CcpOtherConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.decorators.CcpJsonRepresentation.CcpJsonFieldName;
-import com.ccp.business.CcpBusiness;
+import com.ccp.decorators.CcpPropertiesDecorator;
+import com.ccp.decorators.CcpStringDecorator;
 import com.ccp.flow.CcpErrorFlowDisturb;
 import com.ccp.json.validations.global.engine.CcpJsonValidationError;
 import com.ccp.rest.api.spring.servlet.exceptions.CcpErrorExceptionHandlerIsMissing;
@@ -23,7 +27,7 @@ import jakarta.servlet.http.HttpServletResponse;
 @RestControllerAdvice
 public class CcpRestApiExceptionHandlerSpring {
 	enum JsonFieldNames implements CcpJsonFieldName{
-		message
+		message, stackTrace, cause, systems, application_properties
 	}
 
 	public static CcpBusiness genericExceptionHandler;
@@ -62,8 +66,86 @@ public class CcpRestApiExceptionHandlerSpring {
 		if(genericExceptionHandler == null) {
 			throw new CcpErrorExceptionHandlerIsMissing(e);
 		}
+		CcpJsonRepresentation put = getHandledExceptionToLog(e);
+		
+		genericExceptionHandler.apply(put);
+	}
+
+	public static CcpJsonRepresentation getHandledExceptionToLog(Throwable e) {
+		
 		CcpJsonRepresentation json = new CcpJsonRepresentation(e);
-		genericExceptionHandler.apply(json);
+		
+		CcpJsonRepresentation put = getHandledExceptionToLog(json);
+		return put;
+	}
+
+	private static boolean doesNotBelongToDomain(String stack, List<String> systems) {
+		
+		for (String system : systems) {
+			if(stack.contains(system)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static CcpJsonRepresentation getHandledExceptionToLog(CcpJsonRepresentation json) {
+		CcpStringDecorator ccpStringDecorator = new CcpStringDecorator(JsonFieldNames.application_properties.name());
+		CcpPropertiesDecorator propertiesFrom = ccpStringDecorator.propertiesFrom();
+		CcpJsonRepresentation systemProperties = propertiesFrom.environmentVariablesOrClassLoaderOrFile();
+		CcpJsonRepresentation jsonWithStackTrace = getHandledExceptionToLog(json, systemProperties, JsonFieldNames.stackTrace);
+		CcpJsonRepresentation jsonWithCause = getHandledExceptionToLog(jsonWithStackTrace, systemProperties, JsonFieldNames.cause);
+		return jsonWithCause;
+	}
+
+	private static CcpJsonRepresentation getHandledExceptionToLog(CcpJsonRepresentation json,
+			CcpJsonRepresentation systemProperties, JsonFieldNames field) {
+		List<String> stackTrace = json.getAsStringList(field);
+		List<String> newStackTrace = new ArrayList<>();
+		List<String> systems = systemProperties.getAsStringList(JsonFieldNames.systems);
+		int endIndex = stackTrace.size();
+		int startIndex = -1;
+		int index = 0;
+		
+		for (String stack : stackTrace) {
+			
+			boolean doesNotBelongToDomain = doesNotBelongToDomain(stack, systems);
+		
+			if(doesNotBelongToDomain) {
+
+				boolean settingEndIndex = startIndex > -1;
+				
+				if(settingEndIndex) {
+					endIndex = index++;
+					break;
+				}
+				continue;
+			}
+			
+			boolean settingStartIndex = startIndex < 0;
+			
+			if(settingStartIndex) {
+				startIndex = index;
+			}
+
+			index++;
+		}
+		
+		boolean settingEndIndex2 = startIndex > -1;
+		boolean found = settingEndIndex2;
+		
+		if(found) {
+			
+			if(endIndex <  stackTrace.size()) {
+				endIndex++;
+			}
+			
+			newStackTrace = stackTrace.subList(startIndex, endIndex);
+		}
+		
+		CcpJsonRepresentation put = json.put(field, newStackTrace);
+		return put;
 	}
 	
 	@ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
